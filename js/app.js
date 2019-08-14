018f2c32a2668d1a7541af951cc3d58b51c32604
Matt. Toczek <toczek.matt@gmail.com>
2019-08-14 13:11:11 +0100

    span and class added to second li

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
-//1. Add a "featured" tag to the second item in the list: 
+//1. Add a "featured" tag to the second item in the list:
   //  Append a new span element containing the word "Featured" to the list item
+const $featureSpan = $('<span>Featured</span>');
+const $secondLi = $('li').eq(1);
 
-  //  Add a class of "featured" to the new span element. 
+$($secondLi.append($featureSpan));
+  //  Add a class of "featured" to the new span element.
+$($featureSpan.addClass('featured'));
 
+//$($secondLi.append($featureSpan));
 
-//2. Add a class of "new" to the last item in the list. You can traverse to the last item or find a jQuery method that helps you select the last item in the list. 
+//2. Add a class of "new" to the last item in the list. You can traverse to the last item or find a jQuery method that helps you select the last item in the list.
 
-//3. Add an attribute to each link so that all anchor tags open in a new tab. 
+//3. Add an attribute to each link so that all anchor tags open in a new tab.
 
 //4. Select and print the index and text of each anchor tag to the console
 
-//5. Refactor question #4 using the 'this' keyword. 
+//5. Refactor question #4 using the 'this' keyword.
